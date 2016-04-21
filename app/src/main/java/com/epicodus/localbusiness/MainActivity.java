55e9fc5e8708553b7fce959812463685576865c5
Daren Schaad and Megan Fayer <mefayer@gmail.com>
2016-04-21 12:02:01 -0700

    Add a second page with pictures and text

--- app/src/main/java/com/epicodus/localbusiness/MainActivity.java
+++ app/src/main/java/com/epicodus/localbusiness/MainActivity.java
@@ -1,13 +1,36 @@
 package com.epicodus.localbusiness;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.serviceButton)
+    Button mServiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mServiceButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.serviceButton:
+                Intent intent = new Intent(MainActivity.this, ServicesActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
